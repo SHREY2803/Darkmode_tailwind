f7445b57b0a92db9a192ad404c761791bbe56b00
Shrey Kulkarni <shrey280302@gmail.com>
2025-02-05 00:40:48 +0530

    Basc ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <button className="absolute right-10 top-10 bg-black text-white py-3 px-2 rounded-md">DarkMode</button>
+        <h1 className="text-5xl text-center font-extrabold mb-9">
+          Learning Darkmode with Tailwind css
+        </h1>
+        <p className="w-[50%] text-xl text-center mb-9">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          possimus dignissimos veritatis, quod aperiam eos voluptas accusantium
+          nisi sint optio.
         </p>
+        <button className="bg-indigo-700 text-white rounded-xl px-4 py-4 hover:bg-indigo-400 ">
+          Get Started
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
